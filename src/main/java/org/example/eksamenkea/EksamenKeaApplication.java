916f43472a659f727a9dd187770ce87a00467716
malthe-kea <mati0002@stud.kea.dk>
2024-11-15 13:07:38 +0100

    Init commit

---
+++ src/main/java/org/example/eksamenkea/EksamenKeaApplication.java
@@ -0,0 +1,13 @@
+package org.example.eksamenkea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenKeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksamenKeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/eksamenkea/EksamenKeaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamenkea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamenKeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
