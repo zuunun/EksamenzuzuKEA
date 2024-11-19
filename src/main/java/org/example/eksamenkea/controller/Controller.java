00c461977eccb611934a01d06446dc74c823b17c
amaliekea <amje0001@stud.kea.dk>
2024-11-19 14:12:13 +0100

    error handling

--- src/main/java/org/example/eksamenkea/controller/Controller.java
+++ src/main/java/org/example/eksamenkea/controller/Controller.java
@@ -1,7 +1,16 @@
 package org.example.eksamenkea.controller;
 
+import org.example.eksamenkea.service.Errorhandling;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/")
 public class Controller {
+
+    @ExceptionHandler(Errorhandling.class)
+    public String handleError(Model model, Exception exception) {
+        model.addAttribute("message", exception.getMessage());
+        return "error";
+    }
 }
---
+++ src/main/java/org/example/eksamenkea/service/Errorhandling.java
@@ -0,0 +1,7 @@
+package org.example.eksamenkea.service;
+
+public class Errorhandling extends Exception {
+    public Errorhandling(String message) {
+        super(message);
+    }
+}
