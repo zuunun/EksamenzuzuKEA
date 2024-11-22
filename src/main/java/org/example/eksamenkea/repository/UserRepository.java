9feb56c152abc6d8cb5ec10c0bed16475e10a650
amaliekea <amje0001@stud.kea.dk>
2024-11-22 20:10:34 +0100

    disabled userclasses

--- src/main/java/org/example/eksamenkea/repository/UserRepository.java
+++ src/main/java/org/example/eksamenkea/repository/UserRepository.java
@@ -1,47 +1,47 @@
-package org.example.eksamenkea.repository;
-
-import org.example.eksamenkea.model.User;
-import org.example.eksamenkea.service.Errorhandling;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Repository;
-
-import java.sql.*;
-
-@Repository
-public class UserRepository {
-    @Value("${spring.datasource.url}")
-    private String DB_URL;
-
-    @Value("${spring.datasource.username}")
-    private String DB_USER;
-
-    @Value("${spring.datasource.password}")
-    private String DB_PASSWORD;
-
-    //DB_USER=eksamenkea;DB_PASSWORD=Enstorko!;DB_URL=jdbc:mysql://eksamenkeasql.mysql.database.azure.com:3306
-    //DB_USER=root;DB_PASSWORD=amalie;DB_URL=jdbc:mysql://localhost:3306/project_management
-
-    public User signIn(String email, String password) throws Errorhandling{
-        User user = null;
-
-        try {
-            Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-            String SQLUser = "SELECT * FROM user WHERE email = ? AND password = ?;";
-            PreparedStatement pstmt = con.prepareStatement(SQLUser);
-            pstmt.setString(1, email);
-            pstmt.setString(1, password);
-            ResultSet resultSet = pstmt.executeQuery();
-
-            if(resultSet.next()) {
-                int user_id = resultSet.getInt("user_id");
-                int role_id = resultSet.getInt("role_id");
-                user= new User(user_id, email, password, role_id);
-            }
-
-        } catch (SQLException e) {
-            throw new Errorhandling("Sign in error");
-        }
-        return user;
-    }
-
-}
+//package org.example.eksamenkea.repository;
+//
+//import org.example.eksamenkea.model.User;
+//import org.example.eksamenkea.service.Errorhandling;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Repository;
+//
+//import java.sql.*;
+//
+//@Repository
+//public class UserRepository {
+//    @Value("${spring.datasource.url}")
+//    private String DB_URL;
+//
+//    @Value("${spring.datasource.username}")
+//    private String DB_USER;
+//
+//    @Value("${spring.datasource.password}")
+//    private String DB_PASSWORD;
+//
+//    //DB_USER=eksamenkea;DB_PASSWORD=Enstorko!;DB_URL=jdbc:mysql://eksamenkeasql.mysql.database.azure.com:3306
+//    //DB_USER=root;DB_PASSWORD=amalie;DB_URL=jdbc:mysql://localhost:3306/project_management
+//
+//    public User signIn(String email, String password) throws Errorhandling{
+//        User user = null;
+//
+//        try {
+//            Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+//            String SQLUser = "SELECT * FROM user WHERE email = ? AND password = ?;";
+//            PreparedStatement pstmt = con.prepareStatement(SQLUser);
+//            pstmt.setString(1, email);
+//            pstmt.setString(1, password);
+//            ResultSet resultSet = pstmt.executeQuery();
+//
+//            if(resultSet.next()) {
+//                int user_id = resultSet.getInt("user_id");
+//                int role_id = resultSet.getInt("role_id");
+//                user= new User(user_id, email, password, role_id);
+//            }
+//
+//        } catch (SQLException e) {
+//            throw new Errorhandling("Sign in error");
+//        }
+//        return user;
+//    }
+//
+//}
