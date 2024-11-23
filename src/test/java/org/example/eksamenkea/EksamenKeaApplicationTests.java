891b71af48eef3e5864d85c753e0f76a80dee502
amaliekea <amje0001@stud.kea.dk>
2024-11-23 17:17:06 +0100

    kommenteret test ud

--- src/test/java/org/example/eksamenkea/EksamenKeaApplicationTests.java
+++ src/test/java/org/example/eksamenkea/EksamenKeaApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.eksamenkea;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EksamenKeaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package org.example.eksamenkea;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EksamenKeaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
