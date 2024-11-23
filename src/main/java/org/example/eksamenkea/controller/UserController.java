cce89f99b790fac359e50cd77bfa20c0c3bc0762
malthe-kea <mati0002@stud.kea.dk>
2024-11-23 19:51:03 +0100

    Workflow-testasd

--- src/main/java/org/example/eksamenkea/controller/UserController.java
+++ src/main/java/org/example/eksamenkea/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//TEST123
+//aweqwe
 @RequestMapping("")
 @Controller
 public class UserController {
