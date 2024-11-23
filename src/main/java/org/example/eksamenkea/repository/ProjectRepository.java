58b952cd4f2c0df030147b691b540a8e445c7053
“zuunun” <zuun0001@stud.kea.dk>
2024-11-23 15:00:32 +0100

    fjernet security

--- src/main/java/org/example/eksamenkea/repository/ProjectRepository.java
+++ src/main/java/org/example/eksamenkea/repository/ProjectRepository.java
@@ -20,46 +20,46 @@ public class ProjectRepository {
     @Value("${spring.datasource.password}")
     private String DB_PASSWORD;
 
-    public List<Project> getAllProjects() throws SQLException {
-        List<Project> projects = new ArrayList<>();
-        String query = "SELECT * FROM project";
-
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
-
-            while (resultSet.next()) {
-                projects.add(new Project(
-                        resultSet.getInt("project_id"),
-                        resultSet.getString("project_name"),
-                        resultSet.getDate("startdate").toLocalDate(),
-                        resultSet.getDate("enddate").toLocalDate(),
-                        resultSet.getDouble("budget"),
-                        resultSet.getString("project_description"),
-                        resultSet.getInt("projectleader_id")
-                ));
-            }
-        }
-        return projects;
-    }
-
-    public List<Subproject> getAllSubprojects() throws SQLException {
-        List<Subproject> subprojects = new ArrayList<>();
-        String query = "SELECT * FROM subproject";
-
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
-
-            while (resultSet.next()) {
-                subprojects.add(new Subproject(
-                        resultSet.getInt("subproject_id"),
-                        resultSet.getString("subproject_name"),
-                        resultSet.getString("subproject_description"),
-                        resultSet.getInt("project_id")
-                ));
-            }
-        }
-        return subprojects;
-    }
+//    public List<Project> getAllProjects() throws SQLException {
+//        List<Project> projects = new ArrayList<>();
+//        String query = "SELECT * FROM project";
+//
+//        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+//             Statement statement = connection.createStatement();
+//             ResultSet resultSet = statement.executeQuery(query)) {
+//
+//            while (resultSet.next()) {
+//                projects.add(new Project(
+//                        resultSet.getInt("project_id"),
+//                        resultSet.getString("project_name"),
+//                        resultSet.getDate("startdate").toLocalDate(),
+//                        resultSet.getDate("enddate").toLocalDate(),
+//                        resultSet.getDouble("budget"),
+//                        resultSet.getString("project_description"),
+//                        resultSet.getInt("projectleader_id")
+//                ));
+//            }
+//        }
+//        return projects;
+//    }
+//
+//    public List<Subproject> getAllSubprojects() throws SQLException {
+//        List<Subproject> subprojects = new ArrayList<>();
+//        String query = "SELECT * FROM subproject";
+//
+//        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+//             Statement statement = connection.createStatement();
+//             ResultSet resultSet = statement.executeQuery(query)) {
+//
+//            while (resultSet.next()) {
+//                subprojects.add(new Subproject(
+//                        resultSet.getInt("subproject_id"),
+//                        resultSet.getString("subproject_name"),
+//                        resultSet.getString("subproject_description"),
+//                        resultSet.getInt("project_id")
+//                ));
+//            }
+//        }
+//        return subprojects;
+//    }
 }
--- src/main/java/org/example/eksamenkea/repository/TaskRepository.java
+++ src/main/java/org/example/eksamenkea/repository/TaskRepository.java
@@ -21,32 +21,32 @@ public class TaskRepository {
     @Value("${spring.datasource.password}")
     private String DB_PASSWORD;
 
-    public List<Task> getTaskByProjectOrSubProject(int projectId, int subProjectId) throws SQLException {
-        List<Task> tasks = new ArrayList<>();
-        String query = "SELECT t.task_id, t.task_name, t.startdate, t.enddate, t.status, sp.subproject_id " +
-                "FROM task t " +
-                "JOIN subproject sp ON t.subproject_id = sp.subproject_id " +
-                "JOIN project p ON sp.project_id = p.project_id " +
-                "WHERE p.project_id = ? OR sp.subproject_id = ?";
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setObject(1, projectId);
-            preparedStatement.setObject(2, subProjectId);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                tasks.add(new Task(
-                        resultSet.getInt("task_id"),
-                        resultSet.getString("task_name"),
-                        resultSet.getDate("startdate").toLocalDate(),
-                        resultSet.getDate("enddate").toLocalDate(),
-                        Status.valueOf(resultSet.getString("status").toUpperCase()),
-                        resultSet.getInt("subproject_id")
-                ));
-            }
-        }
-        return tasks;
-    }
-
+//    public List<Task> getTaskByProjectOrSubProject(int projectId, int subProjectId) throws SQLException {
+//        List<Task> tasks = new ArrayList<>();
+//        String query = "SELECT t.task_id, t.task_name, t.startdate, t.enddate, t.status, sp.subproject_id " +
+//                "FROM task t " +
+//                "JOIN subproject sp ON t.subproject_id = sp.subproject_id " +
+//                "JOIN project p ON sp.project_id = p.project_id " +
+//                "WHERE p.project_id = ? OR sp.subproject_id = ?";
+//        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+//             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+//            preparedStatement.setObject(1, projectId);
+//            preparedStatement.setObject(2, subProjectId);
 //
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            while (resultSet.next()) {
+//                tasks.add(new Task(
+//                        resultSet.getInt("task_id"),
+//                        resultSet.getString("task_name"),
+//                        resultSet.getDate("startdate").toLocalDate(),
+//                        resultSet.getDate("enddate").toLocalDate(),
+//                        Status.valueOf(resultSet.getString("status").toUpperCase()),
+//                        resultSet.getInt("subproject_id")
+//                ));
+//            }
+//        }
+//        return tasks;
+//    }
+
+
 }
