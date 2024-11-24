f2f78ad953b56e5af00dcdacfc5e261225e6357f
“zuunun” <zuun0001@stud.kea.dk>
2024-11-24 16:21:00 +0100

    getTaskByProjectId

--- src/main/java/org/example/eksamenkea/repository/TaskRepository.java
+++ src/main/java/org/example/eksamenkea/repository/TaskRepository.java
@@ -25,6 +25,30 @@ public class TaskRepository {
     // Hent tasks for et specifikt projekt
     public List<Task> getTasksByProjectId(int projectId) throws SQLException {
         List<Task> tasks = new ArrayList<>();
+        String query = "SELECT t.task_id, t.task_name, t.startdate, t.enddate, t.status, sp.subproject_id " +
+                "FROM task t " +
+                "JOIN subproject sp ON sp.subproject_id = t.subproject_id " +
+                "WHERE sp.project_id = ?";
+
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, projectId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                tasks.add(new Task(
+                        resultSet.getInt("task_id"),
+                        resultSet.getString("task_name"),
+                        resultSet.getDate("startdate").toLocalDate(),
+                        resultSet.getDate("enddate").toLocalDate(),
+                        Status.valueOf(resultSet.getString("status").toUpperCase()),
+                        resultSet.getInt("subproject_id")
+                ));
+
+            }
+        }
 
 
         return tasks;
