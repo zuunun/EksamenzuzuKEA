1ed112e6dd623cb043bd349afef4cc465e4a7e06
malthe-kea <mati0002@stud.kea.dk>
2024-11-25 10:05:08 +0100

    Workflow-test

--- src/main/java/org/example/eksamenkea/repository/interfaces/IProjectRepository.java
+++ src/main/java/org/example/eksamenkea/repository/interfaces/IProjectRepository.java
@@ -2,16 +2,15 @@ package org.example.eksamenkea.repository.interfaces;
 
 import org.example.eksamenkea.model.Project;
 import org.example.eksamenkea.model.Subproject;
-import org.example.eksamenkea.service.Errorhandling;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface IProjectRepository {
 
-    List<Project> getAllProjects() throws Errorhandling;
+    List<Project> getAllProjects() throws SQLException;
 
-    List<Subproject> getAllSubprojects() throws Errorhandling;
+    List<Subproject> getAllSubprojects() throws SQLException;
 
 
 }
