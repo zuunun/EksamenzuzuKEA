5dfc1466ad5ece420deae005051670989aa53f89
amaliekea <amje0001@stud.kea.dk>
2024-11-26 17:34:47 +0100

    test removed

--- src/test/java/org/example/eksamenkea/ProjectApplicationTests.java
+++ src/test/java/org/example/eksamenkea/ProjectApplicationTests.java
@@ -1,17 +1,17 @@
-package org.example.eksamenkea;
-
-
-import org.example.eksamenkea.repository.ProjectRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProjectApplicationTests {
-
-	@Autowired
-	ProjectRepository projectRepository;
-
-
-
-}
+//package org.example.eksamenkea;
+//
+//
+//import org.example.eksamenkea.repository.ProjectRepository;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProjectApplicationTests {
+//
+//	@Autowired
+//	ProjectRepository projectRepository;
+//
+//
+//
+//}
--- src/test/java/org/example/eksamenkea/UserApplicationTests.java
+++ src/test/java/org/example/eksamenkea/UserApplicationTests.java
@@ -1,12 +1,12 @@
-package org.example.eksamenkea;
-
-import org.example.eksamenkea.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UserApplicationTests {
-
-    @Autowired
-    EmployeeRepository employeeRepository;
-}
+//package org.example.eksamenkea;
+//
+//import org.example.eksamenkea.repository.EmployeeRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UserApplicationTests {
+//
+//    @Autowired
+//    EmployeeRepository employeeRepository;
+//}
